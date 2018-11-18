0d6d85316f45b7b86086e733c262421195f9e38d
Andy Fernandez <afgarabote@gmail.com>
2018-11-18 14:17:29 -0500

    some changes

--- flask_swagger_ui/flask_swagger_ui.py
+++ flask_swagger_ui/flask_swagger_ui.py
@@ -3,27 +3,18 @@ import json
 from flask import Blueprint, send_from_directory, render_template, request
 
 
-def get_swaggerui_blueprint(base_url, api_url, config=None, oauth_config=None, blueprint_name='swagger_ui'):
+def get_swaggerui_blueprint(base_url, api_url, oauth_config=None, blueprint_name='swagger_ui'):
     swagger_ui = Blueprint(blueprint_name, __name__, static_folder='dist', template_folder='templates')
-    default_config = {'app_name': 'Swagger UI', 'dom_id': '#swagger-ui', 'url': api_url, 'layout': 'StandaloneLayout'}
-
-    if config:
-        default_config.update(config)
-
-    fields = {
-        'base_url': base_url, 'app_name': default_config.pop('app_name'), 'config_json': json.dumps(default_config),
-    }
+    default_config = dict(url=api_url)
 
     if oauth_config:
-        fields['oauth_config_json'] = json.dumps(oauth_config)
+        default_config.update({'oauth_config_json': json.dumps(oauth_config)})
 
     @swagger_ui.route('/')
     @swagger_ui.route('/<path:path>')
     def show(path=None):
         if not path or path == 'index.html':
-            if not default_config.get('oauth2RedirectUrl', None):
-                default_config.update({"oauth2RedirectUrl": os.path.join(request.base_url, "oauth2-redirect.html")})
-                fields['config_json'] = json.dumps(default_config)
+            fields = dict(base_url=base_url, config=json.dumps(default_config))
             return render_template('index.template.html', **fields)
         else:
             return send_from_directory(os.path.join(swagger_ui.root_path, swagger_ui._static_folder), path)
